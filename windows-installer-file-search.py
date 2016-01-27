8da98af3abdf68dbe42b688ff0aba5e4278a1a19
Christian Funder Sommerlund <zero3@zero3.dk>
2016-01-27 19:54:18 +0100

    Initial commit

---
+++ windows-installer-file-search.py
@@ -0,0 +1,31 @@
+import sys
+import winreg
+
+def search(needle):
+	found = False
+	with winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Installer\\UserData") as userDataParentHandle:
+		for userDataIndex in range(0, winreg.QueryInfoKey(userDataParentHandle)[0]):
+			user = winreg.EnumKey(userDataParentHandle, userDataIndex)
+			with winreg.OpenKey(userDataParentHandle, user) as userDataHandle:
+				with winreg.OpenKey(userDataHandle, "Components") as componentsParentHandle:
+					for componentIndex in range(0, winreg.QueryInfoKey(componentsParentHandle)[0]):
+						with winreg.OpenKey(componentsParentHandle, winreg.EnumKey(componentsParentHandle, componentIndex)) as componentHandle:
+							for valueIndex in range(0, winreg.QueryInfoKey(componentHandle)[1]):
+								valueName, valueData = winreg.EnumValue(componentHandle, valueIndex)[0:2]
+								if needle in valueData:
+									with winreg.OpenKey(userDataHandle, "Products\\" + valueName + "\\InstallProperties") as propertiesHandle:
+										if not found:
+											found = True
+										else:
+											print()
+
+										print("File: " + valueData)
+										print("Product: " + winreg.QueryValueEx(propertiesHandle, "DisplayName")[0])
+										print("Install user: " + user)
+										print("Cached installer: " + winreg.QueryValueEx(propertiesHandle, "LocalPackage")[0])
+
+	if not found:
+		print('No file path containing "' + needle + '" was found in any installed package.')
+
+# Let's go
+search(sys.argv[1])
